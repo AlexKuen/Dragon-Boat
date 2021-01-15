4a4befa3adc878e9e734e88b17f1aca02a4b6ad0
xelaced <47327370+xelaced@users.noreply.github.com>
2021-01-15 11:30:30 +0100

    Add files via upload

--- client.py
+++ client.py
@@ -80,6 +80,17 @@ while start != 'y':
     tmp = receive_message(client_socket)
     if tmp != False:
         start = tmp['data'].decode('utf-8')
+        print("5")
+        time.sleep(1)
+        print("4")
+        time.sleep(1)
+        print("3")
+        time.sleep(1)
+        print("2")
+        time.sleep(1)
+        print("1")
+        time.sleep(1)
+        print("PARTEZ!")
 
 
 ########################################################################
